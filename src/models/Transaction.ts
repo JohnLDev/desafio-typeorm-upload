82100d60d22c22e44ded158671945446105bf659
John Lenon <johnlbeatles@outlook.com>
2020-09-25 21:02:53 -0300

    desafio concluido

---
+++ src/models/Transaction.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+import Category from './Category'
+
+@Entity('transactions')
+class Transaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  title: string
+
+  @Column()
+  type: 'income' | 'outcome'
+
+  @Column()
+  value: number
+
+  @Column()
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category_id: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
+
+export default Transaction
